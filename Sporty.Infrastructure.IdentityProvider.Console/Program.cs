6df6ab27be8d86c9b4343e132168d10daef3e768
manny i. agbayani <serverless.yoda@gmail.com>
2020-03-29 07:39:43 +1300

    added console app for IdentityProvider testing

---
+++ Sporty.Infrastructure.IdentityProvider.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Sporty.Infrastructure.IdentityProvider.Console
+{
+    class Program
+    {
+        static async Task  Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var doc = await httpClient.GetDiscoveryDocumentAsync("http://localhost:51783");
+
+            var tokenResponse = await httpClient.RequestClientCredentialsTokenAsync(
+
+                    new ClientCredentialsTokenRequest()
+                    {
+                        Address = doc.TokenEndpoint,
+                        ClientId = "client",
+                        ClientSecret = "test-test1234",
+                        Scope = "sporty-api"
+                    }
+                ); ;
+
+            System.Console.WriteLine($"Token:  { tokenResponse.AccessToken}");
+              
+        }
+    }
+}
