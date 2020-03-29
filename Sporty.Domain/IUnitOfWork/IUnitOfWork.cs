1df3d4a5c86fd4377a94e0e76a50c344edaff536
manny i. agbayani <serverless.yoda@gmail.com>
2020-03-29 15:42:37 +1300

    added and configure unit of work

---
+++ Sporty.Domain/IUnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Sporty.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sporty.Domain.IUnitOfWork
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        ICategoryRepository Category { get; }
+        IOrderRepository Order { get; }
+        IProductRepository Product { get; }
+        IUserRepository User { get; }
+
+        void Save();
+
+    }
+}
---
+++ Sporty.Infrastructure.Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using Sporty.Domain.EntitiesRepositories;
+using Sporty.Domain.Interfaces;
+using Sporty.Domain.IUnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sporty.Infrastructure.Data.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SportyContext db;
+
+        public UnitOfWork(SportyContext db)
+        {
+            this.db = db;
+            Category = new CategoryRepository(db);
+            Product = new ProductRepository(db);
+            Order = new OrderRepository(db);
+            User = new UserRepository(db);
+        }
+        public ICategoryRepository Category  { get; private set;}
+
+        public IOrderRepository Order { get; private set; }
+
+        public IProductRepository Product { get; private set; }
+
+        public IUserRepository User { get; private set; }
+
+        public void Dispose()
+        {
+            this.db.Dispose();
+        }
+
+        public void Save()
+        {
+            this.db.SaveChanges();
+        }
+    }
+}
